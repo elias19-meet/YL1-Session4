4216b59631809ee44a42968d6ebe3f6d2d91ae47
idane19-meet <idane19@meet.mit.edu>
2017-11-08 19:16:30 +0200

    AYA 4 THE WIN

---
+++ Lab4.py
@@ -0,0 +1,50 @@
+##class Animal(object):
+##    def __init__(self,sound,name,age,favoritecolor):
+##        self.sound=sound
+##        self.name=name
+##        self.age=age
+##        self.favoritecolor=favoritecolor
+##    def eat(self,food):
+##        print("yummy!!"+self.name+" is eating "+food)
+##    def description(self):
+##        print(self.name+" is"+self.age+"years old and loves the color "+self.favoritecolor)
+##    def make_sound(self,times,sound):
+##        print("pumba says :"+sound*times)
+##        
+##pumba=Animal("moo","pumba",7,"brown")
+##
+##pumba.eat("meat")
+##pumba.make_sound(3,"bo ")
+######33333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333
+##class person(object):
+##    def __init__(self,name,age,city,gender):
+##        self.name=name
+##        self.age=age
+##        self.city=city
+##        self.gender=gender
+##    def eat(self,food):
+##        print("yummy!!"+self.name+" is eating "+food)
+##    def playsport(self,sport):
+##        print(self.name+"is playing "+sport)
+##    
+##sam= person("sam",20,"nazareth","other")
+##sam.playsport("swimming")
+#77777777777777777777777777777777777777777777777777777777777777777777777777777777777777777
+
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
